cc7b2902766a9fd45c00a859538bfdb4d59adc54
Zoltan Sisak <zoltan.sisak@gmail.com>
2022-02-21 23:43:49 +0100

    import imgs

--- src/sections/Sponsors/Sponsors.jsx
+++ src/sections/Sponsors/Sponsors.jsx
@@ -2,6 +2,10 @@ import Section from "../../components/Section/Section"
 import Text from "../../components/Text/Text"
 import Title from "../../components/Title/Title"
 import './Sponsors.scss'
+import telekomLogo from "../../assets/img/sponsors/Telekom.png"
+import microsoftLogo from "../../assets/img/sponsors/ms-logo_HU.png"
+import ciscoLogo from "../../assets/img/sponsors/cisco.png"
+import pannonLogo from "../../assets/img/sponsors/pannon-mik.png"
 
 const Sponsor = (props) => {
 	return <a href={props.link} target="_blank" rel="noopener noreferrer" className="sponsor"><img src={props.image} alt={props.name} /></a>
@@ -13,13 +17,13 @@ const Sponsors = () => {
 		<Text subtitle>A mostani rendezvényünk sem jöhetett volna létre az informatika oktatás iránt elkötelezett partnereink együttműködése és támogatása nélkül.</Text>
 		<h3>A rendezvény fő támogatói</h3>
 		<div className="sponsor-grid main-sponsors">
-			<Sponsor image="/assets/img/sponsors/ms-logo_HU.png" link="https://microsoft.hu" />
-			<Sponsor image="/assets/img/sponsors/Telekom.png" link="https://telekom.hu" />
+			<Sponsor image={microsoftLogo} link="https://microsoft.hu" />
+			<Sponsor image={telekomLogo} link="https://telekom.hu" />
 		</div>
 		<h3>Együttműködő partnerek</h3>
 		<div className="sponsor-grid partner-sponsors">
-			<Sponsor image="/assets/img/sponsors/cisco.png" link="http://cisco.hu" />
-			<Sponsor image="/assets/img/sponsors/pannon-mik.png" link="https://mik.uni-pannon.hu/" />
+			<Sponsor image={ciscoLogo} link="http://cisco.hu" />
+			<Sponsor image={pannonLogo} link="https://mik.uni-pannon.hu/" />
 		</div>
 		{/* <div className="sponsor-grid">
 			{(Array(8).fill(1)).map((k,i) => <Sponsor key={i} image="/assets/img/sponsors/google.png" />)}
